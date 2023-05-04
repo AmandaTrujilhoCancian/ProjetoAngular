fa1893eadedfadf9258bf0e3ac7d04e5a8d3b571
AmandaTrujilhoCancian <125562724+AmandaTrujilhoCancian@users.noreply.github.com>
2023-05-04 11:56:23 +0000

    segunda aula

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [ //importa do node_modules, só colocar o nome aqui que ele importa sozinho lá em cima
+    IonicModule,
+    CommonModule 
+  ],
 })
 export class HomePage {
-  constructor() {}
+  // é uma classe
+  item_selecionado: string = 'desenvolvimento_sistemas'; //não tem let
+
+  selecionarCards(event: any){ //não precisa/pode escrever o function antes
+    // tipo de dados any = qualquer um
+    this.item_selecionado = event.detail.value; //coloca o this na frente para pegar a variavel de fora da função
+    console.log(this.item_selecionado);
+  }
 }
